4c352fc2e507f22c2f2c8df92876ef81d20ae2b2
YellaReddyKubernetes <techkubernetes@gmail.com>
2025-06-09 15:09:01 +0530

    Update variables.tf

--- infra-automation-labs/VPC/variables.tf
+++ infra-automation-labs/VPC/variables.tf
@@ -1,5 +1,7 @@
-provider "aws" {
-  region  = var.aws_region
+variable "aws_region" {
+  description = "The AWS region to deploy resources"
+  type        = string
+  default     = "ap-south-1"
 }
 
 variable "vpc_name" {
