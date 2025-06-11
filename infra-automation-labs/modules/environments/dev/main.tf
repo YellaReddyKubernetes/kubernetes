fda93b82693ea12c9b519216ff3700f5401c7612
Reddy <v.yella.reddy@accenture.com>
2025-06-11 13:01:51 +0530

    fmt

--- infra-automation-labs/modules/environments/dev/main.tf
+++ infra-automation-labs/modules/environments/dev/main.tf
@@ -1,10 +1,10 @@
 module "vpc" {
   source = "../../../modules/VPC"
 
-  vpc_name              = var.vpc_name
-  vpc_cidr              = var.vpc_cidr
-  public_subnet_cidrs   = var.public_subnet_cidrs
-  azs                   = var.azs
+  vpc_name            = var.vpc_name
+  vpc_cidr            = var.vpc_cidr
+  public_subnet_cidrs = var.public_subnet_cidrs
+  azs                 = var.azs
 }
 
 provider "aws" {
