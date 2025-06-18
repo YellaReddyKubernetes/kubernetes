7f47880f1e6f8649d28178613baf15827f1b6d34
YellaReddyKubernetes <techkubernetes@gmail.com>
2025-06-18 21:47:07 +0530

    Create eks-cluster.tf

---
+++ infra-automation-labs/modules/eks/eks-cluster.tf
@@ -0,0 +1,121 @@
+provider "aws" {
+  region = var.region
+}
+
+# OIDC Provider for IAM Roles
+data "aws_eks_cluster" "cluster" {
+  name = aws_eks_cluster.eks.name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = aws_eks_cluster.eks.name
+}
+
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "${var.name}-eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "eks.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  for_each = toset([
+    "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
+    "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  ])
+
+  policy_arn = each.value
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+resource "aws_eks_cluster" "eks" {
+  name     = var.name
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = var.private_subnet_ids
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
+}
+
+# IAM Role for EKS Nodes
+resource "aws_iam_role" "eks_node_role" {
+  name = "${var.name}-eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "worker_node_policies" {
+  for_each = toset([
+    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  ])
+
+  policy_arn = each.value
+  role       = aws_iam_role.eks_node_role.name
+}
+
+# EKS Managed Node Group
+resource "aws_eks_node_group" "default" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "${var.name}-node-group"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = var.private_subnet_ids
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  instance_types = ["t3.medium"]
+
+  depends_on = [
+    aws_eks_cluster.eks,
+    aws_iam_role_policy_attachment.worker_node_policies
+  ]
+}
+
+# Store Kubeconfig Locally (Optional)
+resource "local_file" "kubeconfig" {
+  content  = templatefile("${path.module}/kubeconfig.tpl", {
+    cluster_name = aws_eks_cluster.eks.name
+    endpoint     = aws_eks_cluster.eks.endpoint
+    cert_data    = aws_eks_cluster.eks.certificate_authority[0].data
+    token        = data.aws_eks_cluster_auth.cluster.token
+  })
+  filename = "${path.module}/kubeconfig-${var.name}"
+}
+
+# Variables
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "name" {
+  description = "EKS cluster name prefix"
+  default     = "demo-eks"
+}
+
+variable "private_subnet_ids" {
+  type = list(string)
+}
